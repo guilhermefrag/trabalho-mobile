40f6d2d8ad2da7fc3d629b5fdb3baa6a46062842
Guilherme Fragnani <guilhermefragnani@icloud.com>
2023-10-25 23:11:57 -0300

    adicionando model do entretenimento

---
+++ app/src/main/java/com/example/myapplication/database/model/EntretenimentoCalculoModel.java
@@ -0,0 +1,7 @@
+package com.example.myapplication.database.model;
+
+public class EntretenimentoCalculoModel {
+    private String nome;
+    private float custoTotal;
+    private boolean adicionarAoTotal;
+}
